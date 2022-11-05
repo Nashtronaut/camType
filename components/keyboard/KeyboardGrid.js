6ff8eaefdfc4b228ec71fb74b2d6eec400755308
Nashtronaut <fenderfan@live.ca>
2022-11-05 15:11:08 -0600

    added grid component, creating camera holder and grid overlay

--- components/keyboard/KeyboardGrid.js
+++ components/keyboard/KeyboardGrid.js
@@ -1,22 +1,40 @@
-import styles from '../../styles/keyboardGrid.module.css';
-import { Box } from "@mui/material/Box";
+import { useState } from 'react'
+import styles from '../../styles/KeyBoardGrid.module.css'
+import Box from "@mui/material/Box";
 import keyboard from './keyboard.js';
 
 const KeyboardGrid = () => {
+
+    const [keyHeight, setKeyHeight] = useState(100);
+    const [keyWidth, setKeyWidth] = useState(100);
+
+    keyboard.keyHeight = keyHeight;
+    keyboard.keyWidth = keyWidth;
+    const offsets = [30, 50, 70];
+
+    keyboard.rows[0].offset = offsets[0];
+    keyboard.rows[1].offset = offsets[1];
+    keyboard.rows[2].offset = offsets[2];
+
     return (
         <div>
-            {keyboard.map((row) => {
-                <Box className="row" key={row.key}>
-                    <Box width={row.offset} height={row.keyHeight} /> {/* offset Div */}
+            {keyboard.rows.map((row) => {
+                return(
+                    <Box className={styles.row} key={row.id}>
 
-                    {row.map((key) => {
-                        <Box className="key" key={key.key} width={row.keyWidth} height={row.keyHeight}>{key.key.ToUpperCase()}</Box>
-                    })}
+                        <Box className={styles.offset} style={{ width: `${row.offset}px`, height: `${keyboard.keyHeight}px` }}  /> {/* offset Div */}
 
-                </Box>
+                        {row.keys.map((key) => {
+                            return (
+                            <Box className={styles.key} style={{ width: `${keyboard.keyWidth}px`, height: `${keyboard.keyHeight}px` }} key={key.key}>{key.key}</Box>
+                            );
+                        })}
+
+                    </Box>
+                );
             })}
         </div>
     );
 };
 
-export default keyboardGrid;
+export default KeyboardGrid;
