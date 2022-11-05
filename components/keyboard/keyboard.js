c7f2c940e684f134f35b2b4b2bf61ab7f1d8dfdb
LindseaMartin <ninzerb@gmail.com>
2022-11-05 14:10:43 -0600

    finished keyboard

---
+++ components/keyboard/keyboard.js
@@ -0,0 +1,175 @@
+const keyboard = {
+    keyWidth: 10,
+    keyHeight: 10,
+    yOffset: 0,
+    topRow: {
+        key: "topRow",
+        offset: 0,
+        keys: [{
+            q: {
+                key: "q",
+                corsx: [,],
+                corsy: [,]
+            },
+            w: {
+                key: "w",
+                corsx: [,],
+                corsy: [,]
+            },
+            e: {
+                key: "e",
+                corsx: [,],
+                corsy: [,]
+            },
+            r: {
+                key: "r",
+                corsx: [,],
+                corsy: [,]
+            },
+            t: {
+                key: "t",
+                corsx: [,],
+                corsy: [,]
+            },
+            y: {
+                key:"y",
+                corsx: [,],
+                corsy: [,]
+            },
+            u: {
+                key: "u",
+                corsx: [,],
+                corsy: [,]
+            },
+            i: {
+                key: "i",
+                corsx: [,],
+                corsy: [,]
+            },
+            o: {
+                key: "o",
+                corsx: [,],
+                corsy: [,]
+            },
+            p: {
+                key: "p",
+                corsx: [,],
+                corsy: [,]
+            }
+    }]
+    },
+    midRow: {
+        key: "midRow",
+        offset: 0,
+        keys: [{
+            a: {
+                key: "a",
+                corsx: [,],
+                corsy: [,]
+            },
+            s: {
+                key: "s",
+                corsx: [,],
+                corsy: [,]
+            },
+            d: {
+                key: 'd',
+                corsx: [,],
+                corsy: [,]
+            },
+            f: {
+                key: "f",
+                corsx: [,],
+                corsy: [,]
+            },
+            g: {
+                key: "g",
+                corsx: [,],
+                corsy: [,]
+            },
+            h: {
+                key: "h",
+                corsx: [,],
+                corsy: [,]
+            },
+            j: {
+                key: "j",
+                corsx: [,],
+                corsy: [,]
+            },
+            k: {
+                key: "k",
+                corsx: [,],
+                corsy: [,]
+            },
+            l: {
+                key: "l",
+                corsx: [,],
+                corsy: [,]
+            },
+            semi: {
+                key: ":",
+                corsx: [,],
+                corsy: [,]
+            }
+    }]
+    },
+    botRow: {
+        key: "botRow",
+        offset: 0,
+        keys: [{
+            z: {
+                key: "z",
+                corsx: [,],
+                corsy: [,]
+            },
+            x: {
+                key: "x",
+                corsx: [,],
+                corsy: [,]
+            },
+            c: {
+                key: "c",
+                corsx: [,],
+                corsy: [,]
+            },
+            v: {
+                key: "v",
+                corsx: [,],
+                corsy: [,]
+            },
+            b: {
+                key: "b",
+                corsx: [,],
+                corsy: [,]
+            },
+            n: {
+                key: "n",
+                corsx: [,],
+                corsy: [,]
+            },
+            m: {
+                key: 'm',
+                corsx: [,],
+                corsy: [,]
+            },
+            comma: {
+                key: ',',
+                corsx: [,],
+                corsy: [,]
+            },
+            period: {
+                key: '.',
+                corsx: [,],
+                corsy: [,]
+            },
+            slash: {
+                key: "/",
+                corsx: [,],
+                corsy: [,]
+            }
+        }]   
+    }
+};
+
+export default keys; 
