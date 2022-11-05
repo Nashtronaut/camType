526422b9236b726cb8fad83cac084bdce13ff247
Meghan laberge <meghanelaberge@gmail.com>
2022-11-05 15:58:06 -0600

    Started styling

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,6 @@
 import '../styles/globals.css'
+import {ThemeProvider} from "@mui/material"
+import {theme} from "../utils/theme/config"
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
