64aee3f3983dc675a282d8d1a078a6a5837f23f7
ssemeniuk8 <s.semeniuk@me.com>
2022-11-06 05:25:00 -0700

    PlaysInline Error fix

--- components/keyboard/CameraGridContainer.js
+++ components/keyboard/CameraGridContainer.js
@@ -96,7 +96,7 @@ const CameraGridContainer = () => {
             <Grid container style={{ position: 'relative', marginBottom: "1rem" }}>
                 <Grid xs={12} item style={{ display: "flex" }}>
                     <canvas className="output_canvas"  style={{ position: 'absolute', background: "purple", height: "100%", width: "100%"}} />
-                    <video className="input_video" style={{ display: "none" }} autoPlay playInline /> 
+                    <video className="input_video" style={{ display: "none" }} autoPlay playsInline /> 
                     <Box style={{ position: 'relative', width: "100%", height: "100%", display: "flex", zIndex: 1, alignItems: 'end', minHeight: "35rem", maxHeight: "35rem", border: "1px solid hotpink", overflow: 'hidden' }}>
                         <KeyboardGrid
                             keyHeight={keyHeight}
